df5a9cfe5590539f169c6d595f0108d5a48dca49
huijuanli <30285223+huijuanl@users.noreply.github.com>
2018-08-18 15:02:22 +0800

    generate Parenthesis
    这道题给定一个数字n，让生成共有n对括号的所有正确的形式

--- generate_Parenthesis.java
+++ generate_Parenthesis.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 //题目：
 //left:左括号剩余个数
 //right:右括号剩余个数
-//道题给定一个数字n，让生成共有n个括号的所有正确的形式，
+//这道题给定一个数字n，让生成共有n个括号的所有正确的形式，
 // 对于这种列出所有结果的题首先还是考虑用递归Recursion来解，
 // 由于字符串只有左括号和右括号两种字符，而且最终结果必定是左括号3个，右括号3个，
 // 所以我们定义两个变量left和right分别表示剩余左右括号的个数，
@@ -23,11 +23,12 @@ public class generate_Parenthesis {
         if (n < 1)
             return res;
         s = "";
-        process(0, n, n, n);
+//        process(0, n, n);
+        process2(0, n, n);
         return res;
     }
 
-    public void process(int i, int left, int right, int n) {
+    public void process(int i, int left, int right) {
         if (left > right)
             return;
         if (left == 0 && right == 0) {
@@ -36,31 +37,33 @@ public class generate_Parenthesis {
         }
         if (left > 0) {
             s = s + '(';
-            process(i + 1, left - 1, right, n);
+            process(i + 1, left - 1, right);
             s = s.length() == 1 ? "" : s.substring(0, s.length() - 1);
         }
         if (right > 0) {
             s = s + ')';
-            process(i + 1, left, right - 1, n);
+            process(i + 1, left, right - 1);
             s = s.length() == 1 ? "" : s.substring(0, s.length() - 1);
         }
     }
+
     //process()和process2()都可以,process简洁一点，process2更好理解一点
-    public void process2(int i ,int left,int right,int n ){
-        if(left<0||right<0||left>right)
+    public void process2(int i, int left, int right) {
+        if (left < 0 || right < 0 || left > right)
             return;
-        if(i==2*n){
-            if(left==0&&right==0)
-                res.add(s);
+        if (left == 0 && right == 0) {
+            res.add(s);
             return;
         }
-        s = s+'(';
-        process(i+1,left-1,right,n);
-        s = s.length()==1?"": s.substring(0,s.length()-1);
-        s = s+')';
-        process(i+1,left,right-1,n);
-        s = s.length()==1?"": s.substring(0,s.length()-1);
+
+        s = s + '(';
+        process2(i + 1, left - 1, right);
+        s = s.length() == 1 ? "" : s.substring(0, s.length() - 1);
+        s = s + ')';
+        process2(i + 1, left, right - 1);
+        s = s.length() == 1 ? "" : s.substring(0, s.length() - 1);
     }
+
     public static void main(String[] args) {
         generate_Parenthesis it = new generate_Parenthesis();
         ArrayList<String> res = it.generateParenthesis(3);
